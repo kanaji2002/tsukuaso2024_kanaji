53fc8c7c497245c1116102725b00c04c690aa6df
kanaji <gellunika6478@icloud.com>
2024-09-23 04:34:06 +0900

    一旦完成

--- my-app/src/routes/touhoku/+page.svelte
+++ my-app/src/routes/touhoku/+page.svelte
@@ -1,4 +1,159 @@
 <h1> <strong>東北</strong></h1>
 
-<img src="/image/prefecture/touhoku_pre.jpg" alt="Japan Image" />
+
+<!-- <a href="/shikoku">四国</a>
+<a href="/area/hokkaidou">北海道</a> -->
+
+<!-- 画像をリンクとして押せるようにする -->
+
+<style>
+    .image-container {
+      position: relative;
+      display: inline-block; /* 画像のサイズに合わせて親要素のサイズが変わる */
+    }
+  
+    img {
+      width: 100%; /* 画像のサイズを変更する場合 */
+      height: auto;
+    }
+  
+
+
+    .custom-button-aomori {
+      background: url('image/prefecture/aomori.svg');
+      background-size: cover;
+      width:710px;
+      height: 550px;
+      color: white;
+      border: none;
+      font-size: 16px;
+      background: transparent;
+      position: absolute;
+      top: 13%; /* 画像の上からの位置 (全体の30%) */
+      left: 53%; /* 画像の左からの位置 (全体の40%) */
+      transform: translate(-50%, -50%); /* ボタンの中央を基準に調整 */
+    }
+
+
+    .custom-button-iwate {
+      background: url('image/prefecture/iwate.svg');
+      background-size: cover;
+      width: 470px;
+      height: 710px;
+      color: white;
+      border: none;
+      font-size: 16px;
+      background: transparent;
+      position: absolute;
+      top: 42%; /* 画像の上からの位置 (全体の30%) */
+      left: 78%; /* 画像の左からの位置 (全体の40%) */
+      transform: translate(-50%, -50%); /* ボタンの中央を基準に調整 */
+    }
+
+    .custom-button-akita {
+      background: url('image/prefecture/akita.svg');
+      background-size: cover;
+      width: 470px;
+      height: 640px;
+      color: white;
+      border: none;
+      font-size: 16px;
+      background: transparent;
+      position: absolute;
+      top: 40%; /* 画像の上からの位置 (全体の30%) */
+      left: 28%; /* 画像の左からの位置 (全体の40%) */
+      transform: translate(-50%, -50%); /* ボタンの中央を基準に調整 */
+    }
+
+
+
+    .custom-button-miyagi {
+      background: url('image/prefecture/miyagi.svg');
+      background-size: cover;
+      width: 400px;
+      height: 420px;
+      color: white;
+      border: none;
+      font-size: 16px;
+      background: transparent;
+      position: absolute;
+      top: 63%; /* 画像の上からの位置 (全体の30%) */
+      left: 66%; /* 画像の左からの位置 (全体の40%) */
+      transform: translate(-50%, -50%); /* ボタンの中央を基準に調整 */
+    }
+
+
+
+    .custom-button-yamagata {
+      background: url('image/prefecture/yamagata.svg');
+      background-size: cover;
+      width: 380px;
+      height: 450px;
+      color: white;
+      border: none;
+      font-size: 16px;
+      background: transparent;
+      position: absolute;
+      top: 64%; /* 画像の上からの位置 (全体の30%) */
+      left: 23%; /* 画像の左からの位置 (全体の40%) */
+      transform: translate(-50%, -50%); /* ボタンの中央を基準に調整 */
+    }
+
+
+    .custom-button-hukusima {
+      background: url('image/prefecture/hukusima.svg');
+      background-size: cover;
+      width:650px;
+      height: 410px;
+      color: white;
+      border: none;
+      font-size: 16px;
+      background: transparent;
+      position: absolute;
+      top: 88%; /* 画像の上からの位置 (全体の30%) */
+      left: 32%; /* 画像の左からの位置 (全体の40%) */
+      transform: translate(-50%, -50%); /* ボタンの中央を基準に調整 */
+    }
+
+   
+
+
+    .h3dayo {
+text-align: center;
+display: flex;
+justify-content: center;
+font-weight: 500;
+font-size: 25px;}
+  </style>
+  <p class="h3dayo"> 各県を案内します．興味のある<strong>県</strong>を選んでください．</p>
+  <div class="image-container">
+    <!-- 画像を表示 -->
+    <!-- <img src="/path/to/your-image.jpg" alt="Sample Image" />
+   -->
+
+        <img src="/image/prefecture/touhoku_pre.jpg" alt="Japan Image" />
+
+    <a href="/aomori" class="custom-button-aomori"></a>
+    <a href="/iwate" class="custom-button-iwate"></a>
+    <a href="/akita" class="custom-button-akita"></a>
+    <a href="/miyagi" class="custom-button-miyagi"></a>
+    <a href="/yamagata" class="custom-button-yamagata"></a>
+    <a href="/hukusima" class="custom-button-hukusima"></a>
+    
+ 
+    <!-- <a href="/kinnki" class="custom-button-kinnki"></a>
+    <a href="/shikoku" class="custom-button-shikoku"></a>
+    <a href="/tyuugoku" class="custom-button-tyuugoku"></a>
+    <a href="/kyuusyuu" class="custom-button-kyuusyuu"></a>
+    <a href="/okinawa" class="custom-button-okinawa"></a> -->
+
+
+
+    <!-- ボタン1を配置 -->
+    <!-- <button class="button1" on:click={() => alert('Button 1 clicked!')}>Button 1</button> -->
+  
+    <!-- ボタン2を配置 -->
+    
+  </div>
+  
 
